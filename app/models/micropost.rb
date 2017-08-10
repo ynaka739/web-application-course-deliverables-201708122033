df755ef7e52ce0ed7ea4ae0768cd25cb322e3666
Y.N <y.naka.111.739@gmail.com>
2017-08-10 14:31:36 +0000

    201708102331

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,8 +4,7 @@ class Micropost < ApplicationRecord
   has_many :favorites
   
   has_many :retweets, class_name: 'Micropost', foreign_key: 'retweet_id'
-  belongs_to :original, class_name: 'Micropost', foreign_key: 'retweet_id', optional: true
-
+ 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
 end
